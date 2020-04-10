8438629a9ec57f294cb6ae9a7ee875dc1ee79f27
BuildTools <unconfigured@null.spigotmc.org>
2020-04-11 06:50:07 +0800

    live search with both

--- backend-search.php
+++ backend-search.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $userloginname =  $_SESSION['username'];
+$decrypass = $_SESSION['decrypass'];
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "password", "18022038d");
@@ -12,8 +13,15 @@ if($link === false){
  
 if(isset($_REQUEST["term"])){
     // Prepare a select statement
-    $sql = "SELECT * FROM notes WHERE username = '$userloginname' and content LIKE ?";
-   
+    if(!empty($decrypass)){
+    $sql2 = "SELECT noteid, title ,AES_DECRYPT(content,SHA2(CONCAT('$decrypass',salt),256),salt)  
+    FROM notes WHERE username='$userloginname' and encrypted ='1' and AES_DECRYPT(content,SHA2(CONCAT('$decrypass',salt),256),salt) LIKE ?";
+    }
+       
+       $sql = "SELECT noteid, title ,content  
+        FROM notes WHERE username='$userloginname' and content LIKE ?";
+    
+    
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "s", $param_term);
@@ -31,17 +39,40 @@ if(isset($_REQUEST["term"])){
                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
                     echo "<p>" . $row["title"] . "</p>";
                 }
-            } else{
-                echo "<p>No matches found</p>";
-            }
+            } 
         } else{
             echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
         }
     }
+    if(isset($sql2)){
+    if($stmt2 = mysqli_prepare($link, $sql2)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt2, "s", $param_term2);
+        
+        // Set parameters
+        $param_term2 = '%' . $_REQUEST["term"] . '%';
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt2)){
+            $result2 = mysqli_stmt_get_result($stmt2);
+            
+            // Check number of rows in the result set
+            if(mysqli_num_rows($result2) > 0){
+                // Fetch result rows as an associative array
+                while($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
+                    echo "<p>" . $row2["title"] . "</p>";
+                }
+            } 
+        } 
+   
+    }
+    mysqli_stmt_close($stmt2);
+}
+}
      
     // Close statement
     mysqli_stmt_close($stmt);
-}
+   
  
 // close connection
 mysqli_close($link);
